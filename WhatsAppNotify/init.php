3e0500d34ab2153c5fcdbd7209636d5fa42fdaca
Ian Chamba <86791147+ianchamba@users.noreply.github.com>
2024-12-22 14:09:45 -0300

    Delete WhatsAppNotify directory

--- WhatsAppNotify/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined("WHMCS")) {
-    die("Acesso restrito.");
-}
-
-// Inclua o arquivo de hooks
-require_once __DIR__ . '/hooks.php';
-require_once __DIR__ . '/addons/AutoLoginNotify.php';
